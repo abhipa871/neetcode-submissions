4d1bf4d123266b8d49aa1443652d96fa780d53c6
abhipa871 <ap2101@scarletmail.rutgers.edu>
2026-05-29 17:11:42 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        self.min = float('inf')
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if val<=self.min:
+            self.min = val
+            self.min_stack.append(self.min)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.min:
+            self.min_stack.pop()
+            self.min = self.min_stack[-1] if len(self.min_stack)>0 else float('inf')
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
