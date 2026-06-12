742710d1f8cc7d6bc17f7551721f4ea30de27d79
abhipa871 <ap2101@scarletmail.rutgers.edu>
2026-06-12 09:13:13 -0400

    Add: sql-customers-bought-a-b-not-c - submission-5

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-5.sql
@@ -0,0 +1,9 @@
+select customers.customer_id, customers.customer_name
+from customers
+where customers.customer_id in
+(select customer_id from orders where product_name='A')
+and customers.customer_id in
+(select customer_id from orders where product_name='B')
+and customers.customer_id not in
+(select customer_id from orders where product_name='C')
+order by customers.customer_name
