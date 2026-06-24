1c0f6b4e9d4845d5b8240305eca54ae43cb855ac
abhipa871 <ap2101@scarletmail.rutgers.edu>
2026-06-24 16:01:12 -0400

    Update: lowest-common-ancestor-in-binary-search-tree - submission-1

--- Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.py
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.py
@@ -1,42 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
-            return True
-        if not root:
-            return False
-
-        stack = [root]
-
-        while stack:
-            node = stack.pop()
-
-            if node.val == subRoot.val:
-                if self.sameTree(node, subRoot):
-                    return True
-
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-
-        return False
-
-    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        stack = [(root, subRoot)]
-
-        while stack:
-            node1, node2 = stack.pop()
-
-            if not node1 and not node2:
-                continue
-
-            if not node1 or not node2:
-                return False
-
-            if node1.val != node2.val:
-                return False
-
-            stack.append((node1.left, node2.left))
-            stack.append((node1.right, node2.right))
-
-        return True
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if max(p.val, q.val)<root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif min(p.val, q.val)>root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
